0bd54a0b3f0ad2ba3e37ac4bf7c44f07e7449bd5
Thomas Tilak <thomas.tilak@gmail.com>
2023-07-20 20:35:31 +1100

    add some chart types

--- pages/chartpage.tsx
+++ pages/chartpage.tsx
@@ -2,7 +2,7 @@
 
 import { NextPage } from 'next';
 import React, { useEffect, useRef } from 'react';
-import { Spin } from 'antd';
+import { Select, Spin } from 'antd';
 import { useState } from 'react';
 import { SDMXParser } from 'sdmx-json-parser';
 import Chart from '@/pages/component/chart';
@@ -10,57 +10,152 @@ import Chart from '@/pages/component/chart';
 
 const ChartPage: NextPage = () => {
 
-  const [chartType, setChartType] = useState('bar')
-  const [title, setTitle] = useState('Status in employment {$TIME_PERIOD}, [DIN, 15, Bold, Italics, LEFT]')
-  const [unitLoc, setUnitLoc] = useState('hidden')
-  const [decimals, setDecimals] = useState(0)
-  const [labelsYN, setLabelsYN] = useState(true)
-  const [legendConcept, setLegendConcept] = useState('')
-  const [legendLoc, setLegendLoc] = useState('HIDE')
-  const [xAxisConcept, setXAxisConcept] = useState('STE')
-  const [yAxisConcept, setYAxisConcept] = useState('OBS_VALUE')
-  const [downloadYN, setDownloadYN] = useState('')
-  const [url, setUrl] = useState('https://www.ilo.org/sdmx/rest/data/ILO,DF_EMP_TEMP_SEX_AGE_STE_NB,1.0/CHL.A..SEX_T.AGE_YTHADULT_YGE15.STE_ICSE93_6+STE_ICSE93_5+STE_ICSE93_4+STE_ICSE93_3+STE_ICSE93_2+STE_ICSE93_1?endPeriod=2022&lastNObservations=1')
+  const chartConfigs = [{
+    chartType: 'BARS',
+    Title: 'Status in employment {$TIME_PERIOD}, [DIN, 14, Bold, Italics, LEFT]',
+    Subtitle: '',
+    Unit: '',
+    unitLoc: 'HIDE',
+    Decimals: 0,
+    LabelsYN: true,
+    legendConcept: '',
+    legendLoc: 'HIDE',
+    xAxisConcept:  'STE',
+    yAxisConcept: 'OBS_VALUE',
+    downloadYN: '',
+    dataLink: '',
+    metadataLink: '',
+    DATA: "https://www.ilo.org/sdmx/rest/data/ILO,DF_EMP_TEMP_SEX_AGE_STE_NB,1.0/CHL.A..SEX_T.AGE_YTHADULT_YGE15.STE_ICSE93_6+STE_ICSE93_5+STE_ICSE93_4+STE_ICSE93_3+STE_ICSE93_2+STE_ICSE93_1?endPeriod=2022&lastNObservations=1"
+  }, {
+    chartType: 'LINES, double',
+    Title: 'Participation rates,[DIN, 14, Bold, Italics, CENTER]',
+    Subtitle: '',
+    Unit: '',
+    unitLoc: '',
+    Decimals: 0,
+    LabelsYN: false,
+    legendConcept: 'SEX',
+    legendLoc: 'BOTTOM',
+    xAxisConcept: 'TIME_PERIOD',
+    yAxisConcept: 'OBS_VALUE',
+    downloadYN: '',
+    dataLink: '',
+    metadataLink: '', 
+    DATA: "https://www.ilo.org/sdmx/rest/data/ILO,DF_EAP_DWAP_SEX_AGE_RT,1.0/CHL.A..SEX_O+SEX_F+SEX_M+SEX_T.AGE_YTHADULT_YGE15?startPeriod=2010&endPeriod=2022"
+  }, {
+    chartType: 'PIE',
+    Title:  'Employment,[DIN, 14, Bold, Italics, LEFT]',
+    Subtitle: '',
+    Unit: '%',
+    unitLoc: 'SUFFIX',
+    Decimals: 0,
+    LabelsYN:  'Yes',
+    legendConcept: '', 
+    legendLoc: 'HIDE',
+    xAxisConcept: 'ECO',
+    yAxisConcept: '', 
+    downloadYN: '',
+    dataLink: '',
+    metadataLink: '', 
+    DATA: 'https://www.ilo.org/sdmx/rest/data/ILO,DF_EMP_TEMP_SEX_ECO_NB,1.0/CHL.A..SEX_T.ECO_SECTOR_X+ECO_SECTOR_SER+ECO_SECTOR_IND+ECO_SECTOR_AGR?endPeriod=2022&lastNObservations=1'
+  }, {
+    chartType: 'PIE',
+    Title:  'Persons outside the labour force by sex',
+    Subtitle: '{$TIME_PERIOD}',
+    Unit: '%',
+    unitLoc: 'SUFFIX',
+    Decimals: 0,
+    LabelsYN:  'Yes',
+    legendConcept: 'SEX',
+    legendLoc: 'RIGHT',
+    xAxisConcept: null,
+    yAxisConcept: null,
+    downloadYN: null, 
+    dataLink: null,
+    metadataLink: null, 
+    DATA: 'https://www.ilo.org/sdmx/rest/data/ILO,DF_EIP_TEIP_SEX_AGE_NB,1.0/CHL.A..SEX_F+SEX_M.AGE_AGGREGATE_TOTAL?endPeriod=2022&lastNObservations=1&dimensionAtObservation=AllDimensions'
+  }, {
+    chartType: 'PIE',
+    Title:  'Employment by economic activity',
+    Subtitle:  '{$TIME_PERIOD}',
+    Unit: '%',
+    unitLoc: 'SUFFIX',
+    Decimals: 0,
+    LabelsYN:  'Yes',
+    legendConcept: 'ECO',
+    legendLoc: 'HIDE',
+    xAxisConcept: null,
+    yAxisConcept: null,
+    downloadYN: null,
+    dataLink: null,
+    metadataLink: null,
+    DATA: 'https://www.ilo.org/sdmx/rest/data/ILO,DF_EMP_TEMP_SEX_ECO_NB,1.0/CHL.A..SEX_T.ECO_SECTOR_X+ECO_SECTOR_SER+ECO_SECTOR_IND+ECO_SECTOR_AGR?endPeriod=2022&lastNObservations=1'
+  }, {
+    chartType: 'LINES, double',
+    Title: 'Labour Force participation rates',
+    Subtitle: null,
+    Unit: null,
+    unitLoc: null,
+    Decimals: 0,
+    LabelsYN: 'No',
+    legendConcept: 'SEX',
+    legendLoc: 'BOTTOM',
+    xAxisConcept: 'TIME_PERIOD',
+    yAxisConcept: 'OBS_VALUE',
+    downloadYN: null,
+    dataLink: null,
+    metadataLink: null,
+    DATA: 'https://www.ilo.org/sdmx/rest/data/ILO,DF_EAP_DWAP_SEX_AGE_RT,1.0/CHL.A..SEX_O+SEX_F+SEX_M+SEX_T.AGE_YTHADULT_YGE15?startPeriod=2010&endPeriod=2022'
+  }, {
+    chartType: 'BARS',
+    Title:  'Status in employment',
+    Subtitle:  '{$TIME_PERIOD}',
+    Unit: null,
+    unitLoc: 'HIDE',
+    Decimals: 0,
+    LabelsYN: 'Yes',
+    legendConcept: null,
+    legendLoc: 'HIDE',
+    xAxisConcept:  'STE',
+    yAxisConcept: 'OBS_VALUE',
+    downloadYN: null,
+    dataLink: null,
+    metadataLink: null,
+    DATA: 'https://www.ilo.org/sdmx/rest/data/ILO,DF_EMP_TEMP_SEX_AGE_STE_NB,1.0/CHL.A..SEX_T.AGE_YTHADULT_YGE15.STE_ICSE93_6+STE_ICSE93_5+STE_ICSE93_4+STE_ICSE93_3+STE_ICSE93_2+STE_ICSE93_1?endPeriod=2022&lastNObservations=1'
+  }]
 
   const [ready, setReady] = useState(false)
 
   const [chartId, setChartId] = useState('chart-1')
+  const [chartConfig, setChartConfig] = useState(chartConfigs[0])
 
   const [hcOptions, setHcOptions] = useState({})
 
 
   const renderVisual = () =>  {
-    console.log("rendering");
 
-    const chartConfig = {
-      chartType: 'bar',
-      Title: 'Status in employment {$TIME_PERIOD}, [DIN, 14, Bold, Italics, LEFT]',
-      Subtitle: '',
-      Unit: '',
-      unitLoc: 'HIDE',
-      Decimals: 0,
-      LabelsYN: true,
-      legendConcept: '',
-      legendLoc: 'HIDE',
-      xAxisConcept:  'STE',
-      yAxisConcept: 'OBS_VALUE',
-      downloadYN: '',
-      dataLink: '',
-      metadataLink: '',
-      DATA: "https://www.ilo.org/sdmx/rest/data/ILO,DF_EMP_TEMP_SEX_AGE_STE_NB,1.0/CHL.A..SEX_T.AGE_YTHADULT_YGE15.STE_ICSE93_6+STE_ICSE93_5+STE_ICSE93_4+STE_ICSE93_3+STE_ICSE93_2+STE_ICSE93_1?endPeriod=2022&lastNObservations=1"
-    }
+    const selectOptions = chartConfigs.map((config, index) => ({
+      value: index,
+      label: config.Title.split(',')[0]
+    }));
 
+    const defaultChartIndex = 0;
 
     return (
       <div key={`container-${chartId}`}>
+        <Select
+          defaultValue={defaultChartIndex}
+          style={{ width: 420 }}
+          options={selectOptions}
+          onChange={(value: number) => {
+            setReady(false);
+            setChartConfig(chartConfigs[value]);
+          }}
+        />
         <Chart
           config={chartConfig}
           loadedCallback={(state: boolean) => setReady(state)}
         />
-          {/* <HighchartsReact
-            highcharts={Highcharts}
-            options={hcOptions}
-          /> */}
       </div>
     )
   }
--- pages/component/chart.tsx
+++ pages/component/chart.tsx
@@ -1,7 +1,16 @@
-import { HighchartsReact } from "highcharts-react-official";
+import HighchartsReact from "highcharts-react-official";
+import HighchartsExporting from "highcharts/modules/exporting";
+import Accessibility from "highcharts/modules/accessibility";
+import ExportData from "highcharts/modules/export-data";
 import * as Highcharts from 'highcharts';
 import { useEffect, useState } from "react";
 import { SDMXParser } from 'sdmx-json-parser';
+
+if (typeof Highcharts === 'object') {
+    HighchartsExporting(Highcharts);
+    Accessibility(Highcharts);
+    ExportData(Highcharts)
+}
 
 const Chart = ({config, loadedCallback}) => {
 
@@ -49,52 +58,154 @@ const Chart = ({config, loadedCallback}) => {
         return titleObj;
     }
 
+    const processChartExpr = (chartExpr: string) => {
+        const chartType = chartExpr.split(',')[0];
+        switch (chartType) {
+            case 'BARS':
+                return 'bar'
+                break;
+            case 'LINES':
+                return 'line'
+                break;
+            case 'PIE':
+                return 'pie'
+                break;
+            default:
+                break;
+        }
+    }
+
 
     useEffect(() => {
         const url = config.DATA;
         const xAxisConcept = config.xAxisConcept;
-        const chartType = config.chartType;
+        const chartType = processChartExpr(config.chartType);
+        const hcExtraOptions = {};
 
-        sdmxParser.getDatasets(url).then((dataset: any) => {
-            console.log(dataset);
+        sdmxParser.getDatasets(url, {
+            headers: new Headers({
+                Accept: "application/vnd.sdmx.data+json;version=2.0.0",
+            })
+        }).then((dataset: any) => {
             const data = sdmxParser.getData();
             const rawDimensions = sdmxParser.getRawDimensions();
-            const rawDimensionValues = {};
+            const dimensionSingleValues = {};
             rawDimensions
                 .filter((rawDimension: any) => rawDimension.values.length === 1)
                 .forEach((rawDimension: any) => {
-                rawDimensionValues[rawDimension.id] =
+                dimensionSingleValues[rawDimension.id] =
                     rawDimension.values[0].name;
                 });
 
-            const sortedData = sortByDimensionName(data, xAxisConcept);
-            const xAxisValue = sortedData.map((val: any) => {
-                return val[xAxisConcept];
-            });
-            const yAxisValue = sortedData.map((val: any) => {
-                return {
-                ...rawDimensionValues,
-                ...val,
-                y: val["value"],
-                };
-            });
             const titleObj = processTitleExpr(config.Title, rawDimensions);
-            const seriesData = [{
-                name: titleObj.text,
-                data: yAxisValue,
-            },];
+            let seriesData = [];
+            let dataLabels = [];
+            let xAxisValue = [];
+            if(chartType == 'line') {
+                if(config.legendConcept != "") {
+                    const serieDimensions = rawDimensions.find((dimension:any) => dimension.id == config.legendConcept);
+                    if (xAxisConcept == "TIME_PERIOD") {
+                        const timeDimension = rawDimensions.find((dimension:any) => dimension.id == "TIME_PERIOD");
+                        const freqDimension = rawDimensions.find((dimension:any) => dimension.id == "FREQ");
+                        let unit = '';
+                        let dateTimeLabelFormats = {
+                            year: "%Y",
+                            month: "%b",
+                        }
+                        let xAxisLabelformat = '';
+                        if(freqDimension.values[0].id == "A") {
+                            unit = "year";
+                            xAxisLabelformat = "{value:%Y}";
+                        } else if(freqDimension.values[0].id == "Q" || freqDimension.values[0].id == 'M') {
+                            unit = "month";
+                            xAxisLabelformat = "{value:%b %Y}";
+                        }
+                        hcExtraOptions["xAxis"] = {
+                            type: "datetime",
+                            units: [[unit]],
+                            labels: {
+                                format: xAxisLabelformat
+                            }
+                        }
+                    }
+                    serieDimensions.values.forEach((serieDimension:any) => {
+                        const serieData = data.filter((val:any) => val[config.legendConcept] == serieDimension.name);
+                        const sortedData = sortByDimensionName(serieData, xAxisConcept);
+                        const yAxisValue = sortedData.map((val: any) => {
+                            return {
+                            ...dimensionSingleValues,
+                            ...val,
+                            y: val["value"],
+                            x: Date.UTC(val[xAxisConcept].split('-')[0], (val[xAxisConcept].split('-').length > 1 ? val[xAxisConcept][1]: 1), (val[xAxisConcept].split('-').length > 2 ? val[xAxisConcept].split('-')[2]: 1)) 
+                            };
+                        });
+                        seriesData.push({
+                            name: serieDimension.name,
+                            data: yAxisValue
+                        });
+                    });
+                }
+            } else {
+                const sortedData = sortByDimensionName(data, xAxisConcept);
+                xAxisValue = sortedData.map((val: any) => {
+                    return val[xAxisConcept];
+                });
+                hcExtraOptions["xAxis"] = {
+                    categories: xAxisValue
+                }
+                const yAxisValue = sortedData.map((val: any) => {
+                    return {
+                        ...dimensionSingleValues,
+                        ...val,
+                        y: val["value"],
+                        name: (xAxisConcept?val[xAxisConcept]:val[config.legendConcept]),
+                    };
+                });
+
+                if(config.LabelsYN) {
+                    hcExtraOptions["plotOptions"] = {
+                        [chartType]: {
+                            dataLabels: {
+                                enabled: true,
+                                formatter: function() {
+                                    if(config.Unit == '%') {
+                                        if(chartType == "pie") {
+                                            return `${this.point?.name}: ${this.point?.percentage.toFixed(config.Decimals)} %`
+                                        } else {
+                                            return `${this.point?.percentage.toFixed(config.Decimals)} %`
+                                        }
+                                    } else {
+                                        return `${this.point?.y?.toLocaleString()}`
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                seriesData = [{
+                    name: titleObj.text,
+                    data: yAxisValue,
+                },];
+            }
+            // const seriesData = [{
+            //     name: titleObj.text,
+            //     data: yAxisValue,
+            // },];
             loadedCallback(true);
             setHcOptions({
                 chart: {
-                type: chartType,
+                    type: chartType,
                 },
                 title: {
                     text: titleObj.text,
                 },
-                xAxis: {
-                    categories: xAxisValue
+                legend: {
+                    enabled: config.legendLoc == 'HIDE' ? false : true,
+                    align: 'right'//config.legendLoc.toLowerCase()
                 },
-                series: seriesData
+                series:seriesData,
+                ...hcExtraOptions,
             });
         });
     }, [config]);
