8687571ab65ee987cc3cb9a9fdaf7dda833c57cf
stanozr <stanislaso@spc.int>
2023-08-09 11:56:49 +1100

    Replaced Upload API script to validate YAML file and save JSON version (only) in public/uploads

--- app/components/upload/index.tsx
+++ app/components/upload/index.tsx
@@ -7,132 +7,131 @@ import styles from './styles.module.css'
 
 const Upload = () => {
 
-    const [file, setFile] = useState<File | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
 
-    const onFileUploadChange = (e: ChangeEvent<HTMLInputElement>) => {
-      const fileInput = e.target;
-  
-      if (!fileInput.files) {
-        alert("No file was chosen");
-        return;
-      }
-  
-      if (!fileInput.files || fileInput.files.length === 0) {
-        alert("Files list is empty");
-        return;
-      }
-  
-      const file = fileInput.files[0];
-  
-      /** File validation */
-      // if file.type not in a list of allowed types how to write it?
-      if (!["application/x-yaml", "application/x-yml"].includes(file.type)) {
-        alert("Please select a yaml file");
-        return;
-      }
-  
-      /** Setting file state */
-      setFile(file); // we will use the file state, to send it later to the server
-  
-      /** Reset file input */
-      e.currentTarget.type = "text";
-      e.currentTarget.type = "file";
-  
-    };
-  
-    const onCancelFile = (e: MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      if (!file) {
-        return;
-      }
+  const onFileUploadChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const fileInput = e.target;
+
+    if (!fileInput.files) {
+      alert("No file was chosen");
+      return;
+    }
+
+    if (!fileInput.files || fileInput.files.length === 0) {
+      alert("Files list is empty");
+      return;
+    }
+
+    const file = fileInput.files[0];
+
+    /** File validation */
+    // file.type seems empty so let's check for file extension
+    // not reliable but this is a first block
+    if (!/\.(yml|yaml)$/i.test(file.name)) {
+      alert("Please select a YAML file");
+      return;
+    }
+
+    /** Setting file state */
+    setFile(file); // we will use the file state, to send it later to the server
+
+    /** Reset file input */
+    e.currentTarget.type = "text";
+    e.currentTarget.type = "file";
+
+  };
+
+  const onCancelFile = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!file) {
+      return;
+    }
+    setFile(null);
+  };
+
+  const onUploadFile = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!file) {
+      return;
+    }
+
+    try {
+      let formData = new FormData();
+      formData.append("yamlfile", file);
+
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      console.log('API sent response');
+
+      const data = await res.json();
+
+      // console.log('PARSED RESPONSE', res, data);
+
       setFile(null);
-    };
-  
-    const onUploadFile = async (e: MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      if (!file) {
+
+      if (!data || !data.success) {
+        alert(data.error || "Sorry! something went wrong.");
         return;
       }
-  
-      try {
-        let formData = new FormData();
-        formData.append("media", file);
-  
-        const res = await fetch("/api/upload", {
-          method: "POST",
-          body: formData,
-        });
-  
-        const {
-          data,
-          error,
-        }: {
-          data: {
-            url: string | string[];
-          } | null;
-          error: string | null;
-        } = await res.json();
-  
-        if (error || !data) {
-          alert(error || "Sorry! something went wrong.");
-          return;
-        }
-  
-        console.log("File was uploaded successfylly:", data);
-      } catch (error) {
-        console.error(error);
-        alert("Sorry! something went wrong.");
-      }
-    };
 
-    return (<form
-        id={styles.uploadform}
-        className="p-3 border bg-light rounded"
-        action=""
-    >
-        <div className="d-flex flex-column">
-            <label className="d-flex flex-column justify-content-center text-center text-muted">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`${styles.icon} mx-auto`}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
-                    />
-                </svg>
-                <strong>Select an yml file</strong>
-                <input
-                    className="d-none"
-                    name="file"
-                    type="file"
-                    onChange={onFileUploadChange}
-                />
-            </label>
-            <div className="d-flex flex-row justify-content-evenly mt-4">
-                <button
-                    disabled={true}
-                    onClick={onCancelFile}
-                    className="btn btn-outline-secondary rounded"
-                >
-                    Cancel file
-                </button>
-                <button
-                    disabled={!file}
-                    onClick={onUploadFile}
-                    className="btn btn-outline-secondary rounded"
-                >
-                    Upload file
-                </button>
-            </div>
-        </div>
-    </form>)
+      console.log("File was uploaded successfylly:", data.url);
+    } catch (error) {
+      console.error(error);
+      alert("Sorry! something went wrong.");
+    }
+  };
+
+  return (<form
+    id={styles.uploadform}
+    className="p-3 border bg-light rounded"
+    action=""
+  >
+    <div className="d-flex flex-column">
+      <label className={`d-flex flex-column justify-content-center text-center ${file ? 'opacity-75' : 'opacity-50'}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={`${styles.icon} mx-auto`}
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
+          />
+        </svg>
+        <strong>{file ? file.name : "Click to select a file"}</strong>
+        <input
+          className="d-none"
+          name="file"
+          type="file"
+          onChange={onFileUploadChange}
+        />
+      </label>
+      <div className="d-flex flex-row justify-content-evenly mt-4">
+        <button
+          disabled={file ? false : true}
+          onClick={onCancelFile}
+          className="btn btn-outline-secondary rounded"
+        >
+          Cancel file
+        </button>
+        <button
+          disabled={!file}
+          onClick={onUploadFile}
+          className="btn btn-outline-primary rounded"
+        >
+          Upload file
+        </button>
+      </div>
+    </div>
+  </form>)
 
 
 }
--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,50 +1,93 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { parseForm, FormidableError } from "../../lib/parse-form";
-
-
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<{
-    data: {
-      url: string;
-    } | null;
-    error: string | null;
-  }>
-) => {
-  if (req.method !== "POST") {
-    res.setHeader("Allow", "POST");
-    res.status(405).json({
-      data: null,
-      error: "Method Not Allowed",
-    });
-    return;
-  }
-
-  try {
-    const { fields, files } = await parseForm(req);
-
-    console.log({ fields, files });
-
-    res.status(200).json({
-      data: {
-        url: "/uploaded-file-url",
-      },
-      error: null,
-    });
-  } catch (e) {
-    if (e instanceof FormidableError) {
-      res.status(e.httpCode || 400).json({ data: null, error: e.message });
-    } else {
-      console.error(e);
-      res.status(500).json({ data: null, error: "Internal Server Error" });
-    }
-  }
-}
+import { NextApiHandler, NextApiRequest } from "next";
+import formidable from "formidable";
+import path from "path";
+import fs, { promises as fsp } from "fs";
+import YamlValidator from "yaml-validator";
 
 export const config = {
   api: {
     bodyParser: false,
   },
+};
+
+const pathDist: string = path.join(process.cwd(), "/public/uploads");
+
+const readFile = (
+  req: NextApiRequest,
+  saveLocally?: boolean
+): Promise<String> => {
+  const options: formidable.Options = {};
+  if (saveLocally) {
+    options.uploadDir = pathDist;
+    options.filename = (name, ext, path, form) => {
+      return Date.now().toString() + "_" + path.originalFilename;
+    };
+  }
+  options.maxFileSize = 1024 * 100; // 100 KB
+  options.keepExtensions = true;
+
+  console.log("PROCESSING...");
+
+  const form = formidable(options);
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+      }
+      const validator = new YamlValidator({
+        log: true,
+        structure: false,
+        onWarning: null,
+        writeJson: true,
+      });
+
+      // console.log('UPLOADED FILE', files)
+
+      const tmpfilepath = files.yamlfile[0].filepath;
+      const finalname = `${Date.now()}_${files.yamlfile[0].originalFilename}`;
+
+      // console.log("Validating YAML file", finalname);
+      validator.validate([tmpfilepath]);
+
+      // Copy YML file (commented, as  this is not needed)
+      // fs.renameSync(tmpfilepath, `${pathDist}/${finalname}`)
+
+      // delete YML file from tmp folder
+      try {
+        fsp.rm(tmpfilepath);
+      } catch (e) {
+        console.log(e);
+      }
+
+      if (validator.report() > 0) {
+        reject("Not a valid YAML file")
+      } else {
+        // copy JSON file to upload dir
+        const tmpjsonpath = tmpfilepath.replace(/\.y(a)?ml$/iu, ".json")
+        const finaljson = finalname.replace(/\.y(a)?ml$/iu, ".json")
+        fs.renameSync(tmpjsonpath, `${pathDist}/${finaljson}`)
+
+        resolve(`${pathDist}/${finaljson}`)
+      }
+    });
+  });
+};
+
+const handler: NextApiHandler = async (req: NextApiRequest, res) => {
+  // try {
+  //   await fs.readdir(pathDist);
+  // } catch (error) {
+  //   await fs.mkdir(pathDist);
+  // }
+
+  try {
+    const filename = await readFile(req, false)
+    return res
+      .status(200)
+      .json({ success: true, url: filename })
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error })
+  }
 };
 
 export default handler;
