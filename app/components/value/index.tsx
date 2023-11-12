8bfa87ddc8d7a08ed342d93cbe3dcca519d2b5a0
stanozr <stanislaso@spc.int>
2023-11-12 15:41:31 +1100

    Code cleanup: avoid code duplication and use local functions

--- app/components/value/index.tsx
+++ app/components/value/index.tsx
@@ -3,6 +3,7 @@ import { parseOperandTextExpr, parseTextExpr } from '@/app/utils/parseTextExpr';
 // @ts-ignore
 import { SDMXParser } from 'sdmx-json-parser';
 import { parseDataExpr } from "@/app/utils/parseDataExpr";
+import { format } from "path";
 
 const Value = ({ config, loadedCallback }: {config: any, loadedCallback: any}) => {
 
@@ -11,6 +12,21 @@ const Value = ({ config, loadedCallback }: {config: any, loadedCallback: any}) =
     const [valueStr, setValueStr] = useState("Loading...")
 
     const sdmxParser = new SDMXParser();
+
+    const formatValue = (valueStr: any, config: any, data: any, attributes: any) => {
+        if (config['Decimals']) {
+            const decimalNumber = Number(parseOperandTextExpr(config['Decimals'], data[0], attributes));
+            valueStr = Number(valueStr).toFixed(decimalNumber);
+        }
+        if (config['Unit']) {
+            if (config['unitLoc'] == 'SUFFIX') {
+                valueStr += config['Unit'];
+            } else if (config['unitLoc'] == 'PREFIX') {
+                valueStr = config['Unit'] + valueStr;
+            }
+        }
+        return valueStr.toLocaleString();
+    }
 
     useEffect(() => {
         const dataObjs = parseDataExpr(config.DATA);
@@ -41,18 +57,7 @@ const Value = ({ config, loadedCallback }: {config: any, loadedCallback: any}) =
                     // if operand starts with { then it is an attribute
                     const operandValue = parseOperandTextExpr(dataObj.operand, data[0], attributes);
                     valueStr = eval(`${valueStr} ${dataObj.operator} ${operandValue}`);
-                    if (config['Decimals']) {
-                        const decimalNumber = parseOperandTextExpr(config['Decimals'], data[0], attributes);
-                        valueStr = valueStr.toFixed(decimalNumber);
-                    }
-                    if (config['Unit']) {
-                        if (config['unitLoc'] == 'SUFFIX') {
-                            valueStr += config['Unit'];
-                        } else if (config['unitLoc'] == 'PREFIX') {
-                            valueStr = config['Unit'] + valueStr;
-                        }
-                    }
-                    setValueStr(valueStr.toLocaleString());
+                    setValueStr(formatValue(valueStr, config, data, attributes));
                 } else {
                     // we presume it is a dataflow url
                     const parserOperand = new SDMXParser();
@@ -64,34 +69,12 @@ const Value = ({ config, loadedCallback }: {config: any, loadedCallback: any}) =
                         const dataOperand = parserOperand.getData();
                         const dataOperandValue = dataOperand[0].value;
                         valueStr = eval(`${valueStr} ${dataObj.operator} ${dataOperandValue}`);
-                        if (config['Decimals']) {
-                            const decimalNumber = parseOperandTextExpr(config['Decimals'], data[0], attributes);
-                            valueStr = valueStr.toFixed(decimalNumber);
-                        }
-                        if (config['Unit']) {
-                            if (config['unitLoc'] == 'SUFFIX') {
-                                valueStr += config['Unit'];
-                            } else if (config['unitLoc'] == 'PREFIX') {
-                                valueStr = config['Unit'] + valueStr;
-                            }
-                        }
-                        setValueStr(valueStr.toLocaleString());
+                        setValueStr(formatValue(valueStr, config, data, attributes));
                     });
                 }
 
             } else {
-                if (config['Decimals']) {
-                    const decimalNumber = parseOperandTextExpr(config['Decimals'], data[0], attributes);
-                    valueStr = valueStr.toFixed(decimalNumber);
-                }
-                if (config['Unit']) {
-                    if (config['unitLoc'] == 'SUFFIX') {
-                        valueStr += config['Unit'];
-                    } else if (config['unitLoc'] == 'PREFIX') {
-                        valueStr = config['Unit'] + valueStr;
-                    }
-                }
-                setValueStr(valueStr.toLocaleString());
+                setValueStr(formatValue(valueStr, config, data, attributes));
             }
 
             loadedCallback(true)
