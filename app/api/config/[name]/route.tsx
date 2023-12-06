d4c58ad0a39b03e137866d1955fc6fcdcefaba65
Thomas Tilak <thomas.tilak@gmail.com>
2023-12-06 20:47:52 +1100

    update the config API to handle JSON source files and validate them against JSON schemas

--- app/api/config/[name]/route.tsx
+++ app/api/config/[name]/route.tsx
@@ -3,6 +3,7 @@ import path from "path";
 import fs, { promises as fsp } from "fs";
 import yaml from "js-yaml";
 import { loadOneYamlFromGists } from "@/app/utils/loadYamlFromGists";
+import { validate } from "@hyperjump/json-schema/draft-2020-12";
 
 const configFolderPath = path.join(process.cwd(), "/public/uploads");
 
@@ -25,12 +26,22 @@ export async function GET(request: NextRequest, { params }: {params: any}) {
     } else {
         // Load local YAML file
         try {
-            const yamlDoc = await yaml.load(fs.readFileSync(path.join(configFolderPath, `${params.name}.yaml`), "utf8"))
-            return NextResponse.json(yamlDoc)
+            if (params.name.endsWith(".json")){
+                const fileContent = await JSON.parse(fs.readFileSync(path.join(configFolderPath, params.name), "utf8"))
+                const output = await validate("./public/schema/dashboard.schema.json", fileContent)
+                if(output.valid) {
+                    return NextResponse.json(fileContent)
+                } else {
+                    throw new Error("Invalid JSON file: not matching the schema")
+                }
+            } else {
+                const yamlDoc = await yaml.load(fs.readFileSync(path.join(configFolderPath, `${params.name}`), "utf8"))
+                return NextResponse.json(yamlDoc)
+            }
         }
         catch (error) {
             console.error(error)
-            return NextResponse.json({error: `Error loading file ${params.name}.yaml`}, {status: 500})
+            return NextResponse.json({error: `Error loading file ${params.name}`}, {status: 500})
         }
     }
 
