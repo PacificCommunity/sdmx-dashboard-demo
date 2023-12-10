a7fdcb29f7c51048f9e27d7d3f40090996cb5ef5
stanozr <stanislaso@spc.int>
2023-12-10 21:06:23 +1100

    Catching error when server's hostname is not provided in env variables

--- app/utils/getServerHostName.tsx
+++ app/utils/getServerHostName.tsx
@@ -1,5 +1,9 @@
 export default function getServerHostName() {
-    return (process.env.VERCEL_URL ? process.env.VERCEL_URL : (
-        process.env.SERVER_NEXT_PUBLIC_SITE_URL ? process.env.SERVER_NEXT_PUBLIC_SITE_URL : window.location.origin
-    )).replace(/\/$/, "");
+    try {
+        return (process.env.VERCEL_URL ? process.env.VERCEL_URL : (
+            process.env.SERVER_NEXT_PUBLIC_SITE_URL ? process.env.SERVER_NEXT_PUBLIC_SITE_URL : window.location.origin
+        )).replace(/\/$/, "");
+    } catch (e) {
+        return '';
+    }
 }
